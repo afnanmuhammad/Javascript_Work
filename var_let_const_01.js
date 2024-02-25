f385a65dea0e56f9191c6d998d18141fdf8f7069
afnanmuhammad <muhammadafnan849@gmail.com>
2024-02-25 22:21:07 +0500

    Data Types

---
+++ var_let_const_01.js
@@ -0,0 +1,10 @@
+const notchange = 'WMA239486'
+let atchange = 'afnan'
+var everytimechange = 'khan'
+
+//atchange = 'pakistan'
+
+//console.log(notchange);
+
+// we are all data print in same tim only use console.table([data,data,......data])
+console.table([notchange, atchange, everytimechange, atchange]);
