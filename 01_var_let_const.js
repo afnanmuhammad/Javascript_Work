a977a120a709d2cc602b1bd012c44e063b4ed1bf
afnanmuhammad <muhammadafnan849@gmail.com>
2024-03-04 23:05:11 +0500

    first commit

---
+++ 01_var_let_const.js
@@ -0,0 +1,17 @@
+const notchange = 'WMA239486'
+// const can not reassign
+// Const also his block scope like let and it can not be reassigned
+
+
+let atchange = 'Afnan'
+// we can not do this with let because let his only block scope
+// it will show scope error, we only access let within block
+
+var everytimechange = 'khan'
+// var and let can reassigned but const can not be reassigned
+// var scope var his function scope we can use it through out function in any block
+
+//atchange = 'pakistan'
+//console.log(notchange);
+// we are all data print in same tim only use console.table([data,data,......data])
+console.table([notchange, atchange, everytimechange, atchange]);
